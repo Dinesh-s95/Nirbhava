a462443f2d8de2034b69a63e87f8f6422d60a646
Dinesh S <dinesh.s@valtech.com>
2024-10-27 14:15:57 +0530

    test commit to trigger actions

--- src/config/createCollection.js
+++ src/config/createCollection.js
@@ -39,5 +39,3 @@ db.createCollection(collection);
   }
 )*/
 
-// More information on the `createCollection` command can be found at:
-// https://www.mongodb.com/docs/manual/reference/method/db.createCollection/
