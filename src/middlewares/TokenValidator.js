4a1cadfa08e0bf0b809c13b9d5fd8d2112b8cf41
Dinesh S <dinesh.s@valtech.com>
2024-11-15 15:11:30 +0530

    jwt secret excluded

--- src/middlewares/TokenValidator.js
+++ src/middlewares/TokenValidator.js
@@ -2,13 +2,14 @@ const jwt = require('jsonwebtoken');
 
 const protect = (req, res, next) => {
     const token = req.header('Authorization')?.split(' ')[1];
+    const JWT_SECRET = 'JWT_SECRET_PLACEHOLDER'
 
     if (!token) {
         return res.status(401).json({ message: 'No token, authorization denied' });
     }
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, JWT_SECRET);
         req.user = decoded;
         next();
     } catch (err) {
